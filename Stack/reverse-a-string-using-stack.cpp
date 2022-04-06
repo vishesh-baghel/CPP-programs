2a9f33bc7c66b4c2386c956f10899601a9b506f3
Vishesh Baghel <71141511+vishesh-baghel@users.noreply.github.com>
2022-04-06 09:21:11 +0530

    Reverse a string using a stack

--- Stack/reverse-a-string-using-stack.cpp
+++ Stack/reverse-a-string-using-stack.cpp
@@ -1,8 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void reverse(string str) {
+  stack<char> st;
+
+  for (int i = 0; i < str.length(); i++) {
+    st.push(str[i]);
+  }
+
+  while(!st.empty()) {
+    cout << st.top();
+    st.pop();
+  }
+}
 
 int main() {
-
+  string str = "GeeksQuiz";
+    reverse(str);
+  
   return 0;
 }
